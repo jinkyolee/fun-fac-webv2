d98c5f3db1cd59555febbcee9fee4447593a6840
jinkyolee <63771067+jinkyolee@users.noreply.github.com>
2020-10-22 14:08:05 +0900

    Updates

---
+++ precache-manifest.dae16dcb4a1b4753042e401b2eff82ce.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef913767474f91efb2c41f22bff1ee3",
+    "url": "/fun-fac-webv2/index.html"
+  },
+  {
+    "revision": "fce0f603d3039d72742f",
+    "url": "/fun-fac-webv2/static/css/main.e1a1d136.chunk.css"
+  },
+  {
+    "revision": "a343ce934ce6553e2044",
+    "url": "/fun-fac-webv2/static/js/2.3b807966.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fun-fac-webv2/static/js/2.3b807966.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fce0f603d3039d72742f",
+    "url": "/fun-fac-webv2/static/js/main.5dec3038.chunk.js"
+  },
+  {
+    "revision": "75bc55b55bb201cfd602",
+    "url": "/fun-fac-webv2/static/js/runtime-main.13385395.js"
+  },
+  {
+    "revision": "0d42104db1b3f22a62765d59f7820a8a",
+    "url": "/fun-fac-webv2/static/media/content.0d42104d.jpg"
+  },
+  {
+    "revision": "8ff3991ca1d3371955f3de8ccb98d032",
+    "url": "/fun-fac-webv2/static/media/contentImage.8ff3991c.png"
+  },
+  {
+    "revision": "a053ac6701acb878dc5dec8ab3ae6a11",
+    "url": "/fun-fac-webv2/static/media/footer.a053ac67.jpg"
+  },
+  {
+    "revision": "5667e50ce882f76a060246639870f9e6",
+    "url": "/fun-fac-webv2/static/media/gameExplImg.5667e50c.png"
+  },
+  {
+    "revision": "70a4a8d9fbab0281a3a9cd9875e17d62",
+    "url": "/fun-fac-webv2/static/media/homeImage.70a4a8d9.png"
+  },
+  {
+    "revision": "d88a42538b87d2019d5a33a951bb1147",
+    "url": "/fun-fac-webv2/static/media/logo.d88a4253.png"
+  }
+]);
